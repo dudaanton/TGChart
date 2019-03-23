6f88a6ed70867e4acbf2b46f66e5d812512cd875
AntonD <anton@apesong.ru>
2019-03-23 13:54:09 +0300

    rewrite getDate function

--- src/helpers/getDate.js
+++ src/helpers/getDate.js
@@ -1,59 +1,11 @@
-const getMonth = (month) => {
-  switch (month) {
-    case (0):
-      return 'Jan'
-    case (1):
-      return 'Feb'
-    case (2):
-      return 'Mar'
-    case (3):
-      return 'Apr'
-    case (4):
-      return 'May'
-    case (5):
-      return 'Jun'
-    case (6):
-      return 'Jul'
-    case (7):
-      return 'Aug'
-    case (8):
-      return 'Sep'
-    case (9):
-      return 'Oct'
-    case (10):
-      return 'Nov'
-    default:
-      return 'Dec'
-  }
-}
-
-const getWeekDay = (day) => {
-  switch (day) {
-    case (0):
-      return 'Mon'
-    case (1):
-      return 'Tue'
-    case (2):
-      return 'Wed'
-    case (3):
-      return 'Thu'
-    case (4):
-      return 'Fri'
-    case (5):
-      return 'Sat'
-    default:
-      return 'Sun'
-  }
-}
-
 export default (date, weekDay = false) => {
-  const dateObj = new Date()
-  dateObj.setTime(date)
+  const dateObj = new Date(date)
+
   const day = dateObj.getDate()
-  const month = getMonth(dateObj.getMonth())
+  const month = dateObj.toString().substring(4, 7)
 
   if (weekDay) {
-    const dayOfWeek = getWeekDay(dateObj.getDay())
+    const dayOfWeek = dateObj.toString().substring(0, 3)
     return `${dayOfWeek}, ${month} ${day}`
   }
 
